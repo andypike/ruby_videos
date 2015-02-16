03f43cfebc218f8b12bbaab2837dd4e763fd123e
Andy Pike <andy@andypike.com>
2015-02-16 23:26:51 +0000

    Add default image for video and presenter

--- app/concepts/presenters/photo_uploader.rb
+++ app/concepts/presenters/photo_uploader.rb
@@ -8,6 +8,10 @@ module Presenters
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
+    def default_url
+      ActionController::Base.helpers.asset_path("no_image.jpg")
+    end
+
     process :resize_to_fill => [400, 400]
   end
 end
