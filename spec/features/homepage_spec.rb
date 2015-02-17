e97c17a0a043d3f89f29a19aff02fef6d261eb40
Andy Pike <andy@andypike.com>
2015-02-17 23:52:50 +0000

    Only show presenters with published videos on homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,12 +7,19 @@ RSpec.describe "Homepage" do
     expect(page).to have_content(/ruby videos/i)
   end
 
-  it "shows any three presenters" do
-    create_list(:presenter, 5)
+  it "shows presenters with published videos" do
+    create(:presenter_with_published_video, :name => "Andy")
+    create(:presenter_with_published_video, :name => "Mary")
+    create(:presenter_with_draft_video, :name => "Amie")
+    create(:presenter, :name => "Alex")
 
     home_page.open
 
-    expect(home_page.presenters.size).to eq(3)
+    expect(home_page.presenters.size).to eq(2)
+    expect(home_page.presenter("Andy")).to be_present
+    expect(home_page.presenter("Mary")).to be_present
+    expect(home_page.presenter("Amie")).not_to be_present
+    expect(home_page.presenter("Alex")).not_to be_present
   end
 
   it "shows the latest 9 published videos" do
